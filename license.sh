8d98a8c51f359f07244d3be86fe4a53739a08245
Satya <strikersatya2@gmail.com>
2024-11-28 11:30:12 +0530

    Updated license.sh script

--- license.sh
+++ license.sh
@@ -4,14 +4,14 @@ Current_year=2024
 YOB=$1
 Current_age=$(expr $Current_year - $YOB)
 echo "Your Current Age is ${Current_age}."
-if [ Current_age -lt 18 ]; then
+if [ $Current_age -lt 18 ]; then
     echo "You are Not eligible for Driving license"
 elif
-    [ $Current_age -gt 18 -a if $Current_age -lt 50 ]
+    [ $Current_age -gt 18 -a $Current_age -lt 50 ]
 then
     echo "You are Eligible for Driving License."
 elif
-    [ $Current_age -gt 50 -a if $Current_age -lt 100 ]
+    [ $Current_age -gt 50 -a $Current_age -lt 100 ]
 then
     echo "You are Eligible for Driving License, But Fitness and Eyesight Certificates are needed."
 else
